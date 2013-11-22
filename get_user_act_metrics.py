624ac9b224a522c0fb899f543f06d60074b17c0b
JT Huang <jthuang@ischool.berkeley.edu>
2013-11-21 21:36:40 -0800

    Added the code gettting user activity metrics which is missed in the last commit.

--- get_user_act_metrics.py
+++ get_user_act_metrics.py
@@ -4,23 +4,91 @@ import nltk
 from nltk.corpus import stopwords
 import codecs
 import sys
+import json
+import time
+import dateutil.parser
+import datetime
+import calendar
+
+METRICS = ['user', 'commit_cnt', 'commit_add_cnt', 'commit_del_cnt', 'issue_open_cnt', 'issue_close_cnt', 'issue_comment_cnt', 'pull_open_cnt', 'pull_close_cnt', 'pull_comment_cnt']
 
 gram_choice = 'u'
 freq_gram_cnt = 100
 en_stop_words = stopwords.words('english')
 
-def count_act_metrics(user, begin, end):
-  user_act_metrics = {}
-  user_act_metrics['issue_open_cnt'] = 0
-  user_act_metrics['issue_close_cnt'] = 0
-  user_act_metrics['issue_comment_cnt'] = 0
-  user_act_metrics['commit_cnt'] = 0
-  user_act_metrics['commit_add_cnt'] = 0
-  user_act_metrics['commit_del_cnt'] = 0
-  user_act_metrics['commit_comment_cnt'] = 0
+def get_contributor():
+  contributors = []
+  with codecs.open("contributors_summary.txt", 'r',  "UTF-8") as fin:
+    for line in fin:
+      contributors.append(line.split('\t')[0].strip())
 
-  return user_act_metrics
+  return contributors
 
+
+
+def get_user_commit_num(user, begin, end):
+  cnt = add_cnt = del_cnt = 0
+
+  commit_json = open('contributors_all.json')
+  commit_data = json.load(commit_json)
+  commit_json.close()
+  for contributor in commit_data:
+    if contributor['author']['login'] == user:
+      for week in contributor['weeks']:
+        if week['w'] >= begin and week['w'] <= end:
+          cnt += week['c']
+          add_cnt += week['a']
+          del_cnt += week['d']
+
+  return (cnt, add_cnt, del_cnt)
+
+def get_user_pull(user, begin, end, state):
+  pull_cnt = 0
+  fname = "pulls_all_" + state + '.tsv'
+  with codecs.open(fname, 'r',  "UTF-8") as fin:
+    for line in fin:
+      pull_state = line.split('\t')[1].strip()
+      pull_user = line.split('\t')[2].strip()
+      if state == "open": # created_at
+        date_time_obj = dateutil.parser.parse(line.split('\t')[6].strip())
+      else: # closed_at
+        date_time_obj = dateutil.parser.parse(line.split('\t')[7].strip())
+      date_time = calendar.timegm(date_time_obj.utctimetuple())
+
+      if pull_state == state and pull_user == user and date_time >= begin and date_time <= end:
+        pull_cnt += 1
+
+  return pull_cnt
+
+def get_user_issue(user, begin, end, state):
+  issue_cnt = 0
+  fname = "issues_all_" + state + '.tsv'
+  with codecs.open(fname, 'r',  "UTF-8") as fin:
+    for line in fin:
+      issue_state = line.split('\t')[1].strip()
+      issue_user = line.split('\t')[2].strip()
+      if state == "open": # created_at
+        date_time_obj = dateutil.parser.parse(line.split('\t')[7].strip())
+      else: # closed_at
+        date_time_obj = dateutil.parser.parse(line.split('\t')[8].strip())
+      date_time = calendar.timegm(date_time_obj.utctimetuple())
+
+      if issue_state == state and issue_user == user and date_time >= begin and date_time <= end:
+        issue_cnt += 1
+
+  return issue_cnt
+
+def get_user_comment_cnt(user, begin, end, comment_type):
+  fname = comment_type + "s_conversation_details_all.tsv"
+  comment_cnt = 0
+  with codecs.open(fname, 'r',  "UTF-8") as fin:
+    for line in fin:
+      comment_user = line.split('\t')[3].strip()
+      # TODO: no timestamp for comments yet
+      if comment_user == user:
+        comment_cnt += 1
+
+    return comment_cnt
 
 def get_comments():
   comments = []
@@ -31,7 +99,31 @@ def get_comments():
         comments.append(line.split('\t')[2].strip())
 
   return comments
+
+
+
+def count_act_metrics(user, begin, end):
+  user_act_metrics = {}
+  user_act_metrics['user'] = user
+  # TODO: no commit comment yet
+  user_act_metrics['commit_comment_cnt'] = 0
+
+
+  user_act_metrics['pull_comment_cnt'] = get_user_comment_cnt(user, begin, end, "pull")
+  user_act_metrics['pull_open_cnt'] = get_user_pull(user, begin, end, "open")
+  user_act_metrics['pull_close_cnt'] = get_user_pull(user, begin, end, "closed")
+  #print user_act_metrics['pull_comment_cnt'], user_act_metrics['pull_open_cnt'], user_act_metrics['pull_close_cnt']
+
+  user_act_metrics['issue_comment_cnt'] = get_user_comment_cnt(user, begin, end, "issue")
+  user_act_metrics['issue_open_cnt'] = get_user_issue(user, begin, end, "open")
+  user_act_metrics['issue_close_cnt'] = get_user_issue(user, begin, end, "closed")
+  #print user_act_metrics['issue_comment_cnt'], user_act_metrics['issue_open_cnt'], user_act_metrics['issue_close_cnt']
+
+  (user_act_metrics['commit_cnt'], user_act_metrics['commit_add_cnt'], user_act_metrics['commit_del_cnt']) = get_user_commit_num(user, begin, end)
+  #print user_act_metrics['commit_cnt'], user_act_metrics['commit_add_cnt'], user_act_metrics['commit_del_cnt']
   
+  return user_act_metrics
+
 
 def get_freq_unigram(comments, get_stem=False, get_tag=False, flt_stop_words=True):
   unigrams = []
@@ -110,4 +202,32 @@ if __name__=='__main__':
     freq_bigram = get_freq_bigram(comments)
     for bigram in freq_bigram:
       print "({0}, {1})".format(bigram[0], bigram[1])
+  elif gram_choice[0] == 'm':
+    # get all contributors
+    contributors = get_contributor()
 
+    # print out the metrics in first line
+    first = True
+    for metric in METRICS:
+      if first:
+        print metric,
+        first = False
+      else:
+        print '\t' + metric,
+    print
+
+    # print out the metric value for each user
+    for contributor in contributors:
+      contributor_metrics = count_act_metrics(contributor, 0, time.time())
+      first = True
+      for metric in METRICS:
+        if first:
+          print str(contributor_metrics[metric]),
+          first = False
+        else:
+          print '\t' + str(contributor_metrics[metric]),
+      print
+
+
+
+      
